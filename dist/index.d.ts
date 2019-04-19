958805378b5d5204257b4fb5ce3569a5638027b5
Leo Melin <leo.melin@gofore.com>
2019-04-19 14:35:09 +0300

    Fix some typings

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,17 +1,35 @@
-export declare type ActionFunction<T> = {
-    (): T;
+import Vue from 'vue';
+export declare type AnyObject = {
+    [key: string]: any;
+};
+export declare type ActionFunction<T extends AnyObject> = {
+    (...args: any[]): T;
     _$state: T;
 };
-export declare function VxsStore({ state: inputState, getters, actions }: any): {
-    state: any;
+export declare type AnyFunction<T> = (...args: any[]) => T;
+export declare type VxsStoreConstructor<T extends AnyObject> = {
+    state: T;
+    getters: {
+        [key: string]: AnyFunction<any>;
+    };
+    actions: {
+        [key: string]: AnyFunction<T>;
+    };
+};
+export declare function VxsStore<T extends AnyObject>({ state: inputState, getters, actions }: VxsStoreConstructor<T>): {
+    state: T;
     getters: {};
     actions: {};
-    patchState: (patch: any) => any;
+    patchState: (patch: AnyObject) => T & AnyObject;
     getField: (field: string) => () => any;
-    dispatch: (func: any, ...rest: any[]) => Promise<void>;
+    dispatch: (func: ActionFunction<T>, ...rest: any[]) => Promise<void>;
 };
-export declare function Action(actionFunc: any): (target: any, key: string) => void;
-export declare function Get(params: any): (target: any, key: string) => void;
+export declare function Action<T>(actionFunc: ActionFunction<T>): (target: any, key: string) => void;
+export declare type ParametrizedGetter = {
+    getter: AnyFunction<any>;
+    setterAction: ActionFunction<any>;
+};
+export declare function Get(params: AnyFunction<any> | ParametrizedGetter): (target: Vue, key: string) => void;
 export declare type Getter<T> = {
     value: T;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
-import Vue from 'vue'
+import Vue, { ComponentOptions } from 'vue'
 import Component, { createDecorator, mixins } from 'vue-class-component'
 
-export type ActionFunction<T> = {
-  (): T
+export type AnyObject = {
+  [key: string]: any
+}
+
+export type ActionFunction<T extends AnyObject> = {
+  (...args: any[]): T
   _$state: T
 }
 
-export function VxsStore({ state: inputState, getters, actions }: any) {
+export type AnyFunction<T> = (...args: any[]) => T
+
+export type VxsStoreConstructor<T extends AnyObject> = {
+  state: T
+  getters: { [key: string]: AnyFunction<any> }
+  actions: { [key: string]: AnyFunction<T> }
+}
+
+export function VxsStore<T extends AnyObject>({
+  state: inputState,
+  getters,
+  actions
+}: VxsStoreConstructor<T>) {
   // @ts-ignore
   const state = Vue.observable(inputState)
-  const patchState = (patch: any) => {
+  const patchState = (patch: AnyObject) => {
     return {
       ...state,
       ...patch
     }
   }
 
-  const dispatch = async (func: any, ...rest: any[]) => {
+  const dispatch = async (func: ActionFunction<T>, ...rest: any[]) => {
     const newState = await func.apply(null, rest)
     Object.assign(func._$state, newState)
   }
@@ -51,7 +67,7 @@ export function VxsStore({ state: inputState, getters, actions }: any) {
   return store
 }
 
-export function Action(actionFunc: any) {
+export function Action<T>(actionFunc: ActionFunction<T>) {
   return function(target: any, key: string) {
     target[key] = async function(...rest: []) {
       Object.assign(actionFunc._$state, await actionFunc.apply(null, rest))
@@ -59,26 +75,30 @@ export function Action(actionFunc: any) {
   }
 }
 
-export function Get(params: any) {
+export type ParametrizedGetter = {
+  getter: AnyFunction<any>
+  setterAction: ActionFunction<any>
+}
+
+export function Get(params: AnyFunction<any> | ParametrizedGetter) {
   const getterFunction = typeof params === 'function' ? params : params.getter
 
-  return function(target: any, key: string) {
-    createDecorator(function(componentOptions, k): any {
+  return function(target: Vue, key: string) {
+    createDecorator(function(componentOptions, k) {
       componentOptions.inject = {
         ...(componentOptions.inject || {}),
         [k]: {
           default: function() {
-            const vm = this
             return Object.defineProperty(this, k, {
               enumerable: true,
               get() {
                 return getterFunction()
               },
               async set(value) {
-                if (params.setterAction) {
+                if ((<ParametrizedGetter>params).setterAction) {
                   Object.assign(
-                    params.setterAction._$state,
-                    await params.setterAction(value)
+                    (<ParametrizedGetter>params).setterAction._$state,
+                    await (<ParametrizedGetter>params).setterAction(value)
                   )
                 }
               }
